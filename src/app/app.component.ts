7747a97d3e2daf1362f11397550bc35a4e20dd1b
rafaelss <computacao.rafael@gmail.com>
2018-10-18 12:55:03 -0300

    245. Transform Responses Easily with Observable Operators (map())

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import {ServerService} from './server.service';
-import {Response} from '@angular/http';
 
 @Component({
   selector: 'app-root',
@@ -44,10 +43,7 @@ export class AppComponent {
   onGet() {
     this.serverService.getServers()
       .subscribe(
-        (response: Response) => {
-          const data = response.json();
-          console.log(data);
-        },
+        (servers: any[]) => console.log(servers),
         (error) => console.log(error)
       );
   }
--- src/app/server.service.ts
+++ src/app/server.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
-import {Headers, Http} from '@angular/http';
+import {Headers, Http, Response} from '@angular/http';
+import {map} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -19,6 +20,12 @@ export class ServerService {
   }
 
   getServers() {
-    return this.http.get('https://tutorial-angular4-udemy-http.firebaseio.com/data.json');
+    return this.http.get('https://tutorial-angular4-udemy-http.firebaseio.com/data.json')
+      .pipe(map(
+        (response: Response) => {
+          const data = response.json();
+          return data;
+        }
+      ));
   }
 }
